4db7a341ec4031326ba8d6cfb0673153807161b4
Gordon <geniusgordon@gmail.com>
2015-05-25 18:37:59 +0800

    modify arguments help

--- main.py
+++ main.py
@@ -23,22 +23,22 @@ def forum(s, args):
 def post(s, args):
     get_post_detail(s, args.post)
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description='A nthu ilms command line tool')
 subparser = parser.add_subparsers()
 
-p = subparser.add_parser('clist', description='list course')
+p = subparser.add_parser('clist', help='list course')
 p.set_defaults(func=clist)
 
-p = subparser.add_parser('hwlist', description='list homeworks')
+p = subparser.add_parser('hwlist', help='list homeworks')
 p.add_argument('-c', '--course', help='course id')
 p.set_defaults(func=hwlist)
 
-p = subparser.add_parser('forum', description='show course forum')
+p = subparser.add_parser('forum', help='show course forum')
 p.add_argument('-c', '--course', help='course id')
 p.add_argument('-p', '--page', default=1, help='forum page')
 p.set_defaults(func=forum)
 
-p = subparser.add_parser('post', description='show post detail')
+p = subparser.add_parser('post', help='show post detail')
 p.add_argument('-p', '--post', required=True, help='post id')
 p.set_defaults(func=post)
 
